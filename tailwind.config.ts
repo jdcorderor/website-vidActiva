4ee0696363b5e65427fb4ebe173c10d45f56c335
Juan Diego Cordero Román <168385674+jdcorderor@users.noreply.github.com>
2025-08-01 01:51:32 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,15 @@
 module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  safelist: [
+    'w-[22.5rem]',
+    'h-[27.5rem]',
+    'md:w-[24rem]',
+    'md:h-[28.75rem]',
+    'lg:w-[25rem]',
+    'lg:h-[30rem]'
+  ],
   theme: {
     extend: {
       fontFamily: {
